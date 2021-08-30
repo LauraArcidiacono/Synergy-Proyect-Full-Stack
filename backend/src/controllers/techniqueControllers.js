9bab792c252a814619ed3de0a4558b4f2c184002
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-30 19:32:54 +0200

    Add techniqueControllers file

---
+++ backend/src/controllers/techniqueControllers.js
@@ -0,0 +1,66 @@
+const Technique = require('../models/techniqueModel');
+
+async function getAllTechniques({ query }, res) {
+  try {
+    const techniques = await Technique.find(query);
+    res.json(techniques);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+async function createOneTechnique({ body }, res) {
+  try {
+    const newTechnique = await Technique.create(body);
+    res.json(newTechnique);
+  } catch (error) {
+    res.status(500);
+    res.send('The Technique hasn´t been created');
+  }
+}
+
+async function getOneTechniqueById({ params: { techniqueId } }, res) {
+  try {
+    const foundTechnique = await Technique.findById(techniqueId);
+    res.json(foundTechnique);
+    res.send(204);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
+async function updateOneTechniqueById(req, res) {
+  const { techniqueId } = req.params;
+  const dataToUpdate = req.body;
+  try {
+    const updatedTechnique = await Technique.findByIdAndUpdate(
+      techniqueId,
+      dataToUpdate,
+      { new: true }
+    );
+    res.json(updatedTechnique);
+  } catch (error) {
+    res.status(500);
+    res.send('The Technique hasn´t been updated');
+  }
+}
+
+async function deleteOneTechniqueById(req, res) {
+  const { techniqueId } = req.params;
+  try {
+    await Technique.findByIdAndDelete(techniqueId);
+    res.json('The Technique has been deleted');
+  } catch (error) {
+    res.status(500);
+    res.send('The Technique hasn´t been deleted');
+  }
+}
+
+module.exports = {
+  getAllTechniques,
+  createOneTechnique,
+  getOneTechniqueById,
+  updateOneTechniqueById,
+  deleteOneTechniqueById
+};
