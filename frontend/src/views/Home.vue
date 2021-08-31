8643d3d3cf7bfe8983ac790ce6df32738c6c5c1b
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-31 20:06:26 +0200

    Change HelloWorld msg

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Laura's Project With VUE"/>
   </div>
 </template>
 
