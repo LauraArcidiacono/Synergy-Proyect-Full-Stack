38e3ac093e352945d6b5d924356430c4194f6dfa
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-08-31 19:19:12 +0200

    Protect all routes with passport.authenticate

--- backend/index.js
+++ backend/index.js
@@ -2,31 +2,36 @@ require('dotenv').config();
 const express = require('express');
 const debug = require('debug')('synergy');
 const morgan = require('morgan');
+const passport = require('passport');
+const cors = require('cors');
 const chalk = require('chalk');
 
+require('dotenv').config();
 require('./src/configDDBB/configDDBB');
+require('./src/auth/auth');
 
 const server = express();
 const port = process.env.PORT || 5000;
 
 server.use(express.json());
 server.use(morgan('dev'));
+server.use(cors());
 
-const userRouter = require('./src/routes/userRouter');
+const authRoutes = require('./src/routes/authRouter');
+const userProtectedRoutes = require('./src/routes/userRouter');
+const techniqueProtectedRoutes = require('./src/routes/techniqueRouter');
+const reviewProtectedRoutes = require('./src/routes/reviewRoutes');
+const resourceProtectedRoutes = require('./src/routes/resourceRouter');
 
-server.use('/synergy/users', userRouter);
+server.use('/synergy/auth/', authRoutes);
 
-const techniqueRouter = require('./src/routes/techniqueRouter');
+server.use('/synergy/users', passport.authenticate('jwt', { session: false }), userProtectedRoutes);
 
-server.use('/synergy/technique', techniqueRouter);
+server.use('/synergy/technique', passport.authenticate('jwt', { session: false }), techniqueProtectedRoutes);
 
-const reviewRouter = require('./src/routes/reviewRoutes');
+server.use('/synergy/review', passport.authenticate('jwt', { session: false }), reviewProtectedRoutes);
 
-server.use('/synergy/review', reviewRouter);
-
-const resourceRouter = require('./src/routes/resourceRouter');
-
-server.use('/synergy/resource', resourceRouter);
+server.use('/synergy/resource', passport.authenticate('jwt', { session: false }), resourceProtectedRoutes);
 
 server.listen(
   port,
