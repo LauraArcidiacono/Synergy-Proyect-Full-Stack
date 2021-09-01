e7b6c60b7b600a3c5815e616818cb3abdeb5b5ba
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-09-01 23:06:49 +0200

    Add Resources.vue file

---
+++ frontend/src/views/Resources.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h2>This is the RESOURCE page</h2>
+    <p>Aquí se verán recursos bibliográficos</p>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'Resources'
+};
+</script>
+
+<style>
+
+</style>
