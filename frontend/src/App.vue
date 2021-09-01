307566103708ab7679552c34a2c579021fe74de6
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-09-01 23:04:10 +0200

    Refactor App.vue
    -Add router-link to: techniques, profile, resources

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/techniques">Techniques</router-link> |
+    <router-link to="/profile">Profile</router-link> |
+    <router-link to="/resources">Resources</router-link> 
   </div>
   <router-view/>
 </template>
