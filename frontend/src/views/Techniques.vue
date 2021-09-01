23c4ce1bbc6704d3092ac1405855c18e065e3288
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-09-01 23:07:01 +0200

    Add Techniques.vue file

---
+++ frontend/src/views/Techniques.vue
@@ -0,0 +1,16 @@
+<template>
+   <div class="about">
+    <h2>This is the TECHNIQUES page</h2>
+    <p>Aquí se verá la lista de técnicas</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Techniques'
+};
+</script>
+
+<style>
+
+</style>
