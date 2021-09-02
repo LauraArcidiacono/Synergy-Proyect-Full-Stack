92c3d1668fbd1eae4b672fb37bcd6d36b8d96588
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-09-02 20:39:26 +0200

    Add TechniqueCard as component to Techniques.vue

--- frontend/src/views/Techniques.vue
+++ frontend/src/views/Techniques.vue
@@ -2,12 +2,18 @@
   <div class="about">
     <h2>This is the TECHNIQUES page</h2>
     <p>Aquí se verá la lista de técnicas</p>
+
+    <TechniqueCard />
   </div>
 </template>
 
 <script>
+import TechniqueCard from "../components/TechniqueCard.vue";
 export default {
   name: "Techniques",
+  components: {
+    TechniqueCard,
+  },
 };
 </script>
 
