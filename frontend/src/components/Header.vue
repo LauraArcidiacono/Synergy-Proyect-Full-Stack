47bb44035829b387b31c95bafdf7513cc8ff52e2
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-09-02 12:29:02 +0200

    Create and style Header component

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <header class="header">
+    <img
+      class="header__logo"
+      src="../images/LogoSynergy.png"
+      alt="Logo Synergy"
+    />
+    <div class="header__nav">
+      <router-link to="/techniques" class="nav__item">Techniques</router-link>
+      <router-link to="/profile" class="nav__item">Profile</router-link>
+      <router-link to="/resources" class="nav__item">Resources</router-link>
+      <router-link to="/loginRegister" class="nav__item">Ingresar</router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+header {
+  display: flex;
+  height: 30vh;
+  background-color: rgb(215, 234, 226);
+  align-items: center;
+  justify-content: space-between;
+}
+.header__logo {
+  height: 90%;
+  margin: 1vh;
+}
+.header__nav {
+  margin: 3vh;
+  align-items: center;
+  justify-items: flex-end;
+}
+.nav__item {
+  margin: 3vw;
+  text-decoration: none;
+  color: rgb(10, 76, 99);
+  font-family: "Poppins", sans-serif;
+}
+</style>
