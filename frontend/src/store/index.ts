b8d8d76325ad923113f45a271ab305a329b99d13
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-09-03 09:31:08 +0200

    Add new token to fetchTechniquesFromApi

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -12,7 +12,7 @@ export default createStore({
   },
   actions: {
     async fetchTechniquesFromApi({commit}) {
-      const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjYxMmU1NmM4NTdiZmE5Mjc0NjUzZWYyNCIsImVtYWlsIjoibGF1cmFAZ21haWwuY29tIn0sImlhdCI6MTYzMDYwMzU2MCwiZXhwIjoxMjE2MzA2MDM1NjB9.1m-3zHE5SpSW6NkpdBOrOJCivrQYmAtZUiKm_O9FJhM'
+      const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjYxMmU1NmM4NTdiZmE5Mjc0NjUzZWYyNCIsImVtYWlsIjoibGF1cmFAZ21haWwuY29tIn0sImlhdCI6MTYzMDYxNzExOCwiZXhwIjoxMjE2MzA2MTcxMTh9.xd8eCKBmycHw8PthB4WTm0rIfG1ypH0wGw2QR39LXes'
       const { data } = await axios({
         method: 'GET',
         url: 'http://localhost:5000/synergy/technique',
