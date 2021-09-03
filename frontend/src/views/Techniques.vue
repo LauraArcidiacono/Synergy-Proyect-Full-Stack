5d5075e23166857fa5309849f8bd551bae354433
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-09-03 12:47:37 +0200

    Refactor Techniques and TechniquesCard
    -Move  ul/li logic from TechniqueCard component to Technique view

--- frontend/src/views/Techniques.vue
+++ frontend/src/views/Techniques.vue
@@ -2,20 +2,51 @@
   <div class="about">
     <h2>This is the TECHNIQUES page</h2>
     <p>Aquí se verá la lista de técnicas</p>
-
-    <TechniqueCard />
+    <ul class="technique__card">
+      <li
+        v-for="technique in techniques"
+        :key="technique._id"
+        class="card__item"
+      >
+        <TechniqueCard
+          :name="technique.name"
+          :type="technique.type"
+          :goal="technique.goal"
+          :ilustration="technique.ilustration"
+        />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import { mapActions, mapState } from "vuex";
 import TechniqueCard from "../components/TechniqueCard.vue";
 export default {
   name: "Techniques",
   components: {
     TechniqueCard,
   },
+  computed: {
+    ...mapState(["techniques"]),
+  },
+  methods: {
+    ...mapActions(["fetchTechniquesFromApi"]),
+  },
+  mounted() {
+    this.fetchTechniquesFromApi();
+  },
 };
 </script>
 
-<style>
+<style lang="scss">
+@import "../styles/_colors.scss";
+@import "../styles/_mixins.scss";
+.technique__card {
+  display: flex;
+  flex-direction: row;
+}
+.card__item {
+  @include technique__item;
+}
 </style>
