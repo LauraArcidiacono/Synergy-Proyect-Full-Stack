c328907f369b239b562b39f25842100b3f96a366
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-09-03 12:48:57 +0200

    Refactor routes and NotFound view
    -Add /synergy to routes in index.ts
    
    -Add NotFound view

---
+++ frontend/src/views/NotFound.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="notFound">
+    <h2>404- Page Not Found</h2>
+    <button class="button">Volver</button>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang='scss'>
+@import "../styles/_colors.scss";
+@import "../styles/_mixins.scss";
+
+.notFound {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10rem;
+}
+
+.button {
+  @include button;
+}
+</style>
