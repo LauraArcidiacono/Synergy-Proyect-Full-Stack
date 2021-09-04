663329c5c708def4649d41cad27819b4cf1577e3
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-09-04 21:19:42 +0200

    Create Resources view

--- backend/src/models/resourceModel.js
+++ backend/src/models/resourceModel.js
@@ -1,11 +1,11 @@
 const { model, Schema } = require('mongoose');
 
 const resourceSchema = Schema({
- title: String,
- author: String,
- editorial: String,
- siteEditorial: String,
- image: String
-})
+  title: String,
+  author: String,
+  editorial: String,
+  siteEditorial: String,
+  image: String
+});
 
 module.exports = model('Resource', resourceSchema);
