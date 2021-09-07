cbec2eaf7177ce77bc519f66300d6fbe6ddf2fdf
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-09-07 18:47:16 +0200

    Remove actions and state
    -Moveactions and state to independent files

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,16 +1,10 @@
 import { createStore } from 'vuex';
-import axios from 'axios';
+import actions from '@/store/actions';
+import state from '@/store/state';
+
 
 export default createStore({
-  state: {
-    techniques: [],
-    currentTechnique: {},
-    currentUser: {},
-    isUserAuthenticate: false,
-    token: "",
-    refreshToken: "",
-    resources: [],
-  },
+  state,
   mutations: {
     loadTechniques(state, payload) {
       state.techniques = payload;
@@ -28,7 +22,6 @@ export default createStore({
       state.isUserAuthenticate = !state.isUserAuthenticate;
       state.token = payload.token;
       state.refreshToken = payload.refreshToken;
-      state.currentUser = payload.user;
     },
     
     loadUser(state, payload) {
@@ -36,53 +29,7 @@ export default createStore({
     }
 
   },
-  actions: {
-    async fetchTechniquesFromApi({commit}) {
-      const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjYxMmU1NmM4NTdiZmE5Mjc0NjUzZWYyNCIsImVtYWlsIjoibGF1cmFAZ21haWwuY29tIn0sImlhdCI6MTYzMDg0OTAwMiwiZXhwIjoxMjE2MzA4NDkwMDJ9.t_rHvJjmgHKDtD-vRWeujhl9f7tj_MCpSHSoxGfP3_M'
-      const { data } = await axios({
-        method: 'GET',
-        url: 'http://localhost:5000/synergy/technique',
-        headers: { Authorization: `Bearer ${token}` }
-      }); 
-      commit('loadTechniques', data);
-    },
-
-    async fetchOneTechniqueFromApi({commit}, id) {
-      const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjYxMmU1NmM4NTdiZmE5Mjc0NjUzZWYyNCIsImVtYWlsIjoibGF1cmFAZ21haWwuY29tIn0sImlhdCI6MTYzMDg0OTAwMiwiZXhwIjoxMjE2MzA4NDkwMDJ9.t_rHvJjmgHKDtD-vRWeujhl9f7tj_MCpSHSoxGfP3_M'
-      const { data } = await axios({
-        method: 'GET',
-        url: `http://localhost:5000/synergy/technique/${id}`,
-        headers: { Authorization: `Bearer ${token}` }
-      }); 
-      commit('loadOneTechnique', data);
-      console.log(data)
-    },
-
-    async userLogedFromApi({commit}, currentUser) {
-      console.log(currentUser.token);
-      const {token} = currentUser;
-      const {userId} = currentUser;
-      console.log(userId);
-      const { data } = await axios({
-        method: 'GET',
-        url: `http://localhost:5000/synergy/users/${userId}`,
-        headers: { Authorization: `Bearer ${token}` }
-      });
-      commit('loadUser', data);
-    },
-
-    async fetchResourcesFromApi({commit}, token) {
-      console.log("este es el token", token)
-      const { data } = await axios({
-        method: 'GET',
-        url: 'http://localhost:5000/synergy/resource',
-        headers: { Authorization: `Bearer ${token}`}
-      });
-      commit('loadResources', data);
-    },
-
-
-  },
+  actions,
   modules: {
   },
 });
