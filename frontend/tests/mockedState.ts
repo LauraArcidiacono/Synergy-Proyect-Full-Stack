8376b93f9d57d938109ea237fd77e8bcbc0f0928
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-09-10 20:39:29 +0200

    Add mockedState file to test

---
+++ frontend/tests/mockedState.ts
@@ -0,0 +1,168 @@
+const stateMock = {
+    techniques: [{
+        _id: "61314337f8aab565c3e56087",
+        name: "¿Te gusta tu compañia?",
+        type: "Animación",
+        time: 30,
+        goal: "Caldeamiento y animación del grupo.",
+        material: "Sillas en ronda.",
+        task: "Se forma una ronda de sillas y una persona queda...",
+        workflow: "Dinamiza y anima al grupo, genera muchas risas, complicidades, movimiento.",
+        ilustration: "https://i.ibb.co/qptF73V/Music-Celebrate.png",
+        reviews: [{
+                _id: "612deae3ae4b43e9447880fb",
+                technique: {
+                    _id: "612df3d8dbe5775944437e46",
+                    name: "Planificar eventos",
+                    type: "Planificación",
+                    time: 25,
+                    goal: "Optimizar el trabajo del equipo",
+                    material: "Postit y bolis",
+                    task: "Crear una lista de eventos",
+                    workflow: "Armar grupos de tres personas. Proponer eventos.",
+                    ilustration:"https://i.ibb.co/t8wj9Yr/Finances.png",
+                    reviews: [
+                        "612deae3ae4b43e9447880fb",
+                    ],
+                },
+                user: {
+                    _id: "612cdf2cfa97f2e5c4f1f107",
+                    name:"Jordi",
+                    profession: "Sensei",
+                    city: "Vic",
+                    avatar: "https://i.ibb.co/GPnrGDw/Default-profile-picture-avatar-photo-placeholder-Vector-illustration.jpg",
+                    email: "jordi.sensei@gmail.com",
+                    password: "jordi1234",
+                    favoriteTechniques: [
+                        "613140ccf8aab565c3e56081",
+                    ],
+                    techniquesProvided: [],
+                },
+                description: "Esta técnica es excelente para revisar...",
+                score: 5,
+        }],
+        userProvider: "6134eec9c158ae21f6cd95d4" 
+        }],
+
+    currentTechnique: {
+        _id: "61314337f8aab565c3e56087",
+        name: "¿Te gusta tu compañia?",
+        type: "Animación",
+        time: 30,
+        goal: "Caldeamiento y animación del grupo.",
+        material: "Sillas en ronda.",
+        task: "Se forma una ronda de sillas y una persona queda...",
+        workflow: "Dinamiza y anima al grupo, genera muchas risas, complicidades, movimiento.",
+        ilustration: "https://i.ibb.co/qptF73V/Music-Celebrate.png",
+        reviews: [{
+                _id: "612deae3ae4b43e9447880fb",
+                technique: {
+                    _id: "612df3d8dbe5775944437e46",
+                    name: "Planificar eventos",
+                    type: "Planificación",
+                    time: 25,
+                    goal: "Optimizar el trabajo del equipo",
+                    material: "Postit y bolis",
+                    task: "Crear una lista de eventos",
+                    workflow: "Armar grupos de tres personas. Proponer eventos.",
+                    ilustration:"https://i.ibb.co/t8wj9Yr/Finances.png",
+                    reviews: [
+                        "612deae3ae4b43e9447880fb",
+                    ],
+                },
+                user: {
+                    _id: "612cdf2cfa97f2e5c4f1f107",
+                    name:"Jordi",
+                    profession: "Sensei",
+                    city: "Vic",
+                    avatar: "https://i.ibb.co/GPnrGDw/Default-profile-picture-avatar-photo-placeholder-Vector-illustration.jpg",
+                    email: "jordi.sensei@gmail.com",
+                    password: "jordi1234",
+                    favoriteTechniques: [
+                        "613140ccf8aab565c3e56081",
+                    ],
+                    techniquesProvided: [],
+                },
+                description: "Esta técnica es excelente para revisar...",
+                score: 5,
+        }],
+        userProvider: "6134eec9c158ae21f6cd95d4" 
+        },
+
+    currentUserTechniquesProvided: [{
+        _id: "61314337f8aab565c3e56087",
+        name: "¿Te gusta tu compañia?",
+        type: "Animación",
+        time: 30,
+        goal: "Caldeamiento y animación del grupo.",
+        material: "Sillas en ronda.",
+        task: "Se forma una ronda de sillas y una persona queda...",
+        workflow: "Dinamiza y anima al grupo, genera muchas risas, complicidades, movimiento.",
+        ilustration: "https://i.ibb.co/qptF73V/Music-Celebrate.png",
+        reviews: [{
+                _id: "612deae3ae4b43e9447880fb",
+                technique: {
+                    _id: "612df3d8dbe5775944437e46",
+                    name: "Planificar eventos",
+                    type: "Planificación",
+                    time: 25,
+                    goal: "Optimizar el trabajo del equipo",
+                    material: "Postit y bolis",
+                    task: "Crear una lista de eventos",
+                    workflow: "Armar grupos de tres personas. Proponer eventos.",
+                    ilustration:"https://i.ibb.co/t8wj9Yr/Finances.png",
+                    reviews: [
+                        "612deae3ae4b43e9447880fb",
+                    ],
+                },
+                user: {
+                    _id: "612cdf2cfa97f2e5c4f1f107",
+                    name:"Jordi",
+                    profession: "Sensei",
+                    city: "Vic",
+                    avatar: "https://i.ibb.co/GPnrGDw/Default-profile-picture-avatar-photo-placeholder-Vector-illustration.jpg",
+                    email: "jordi.sensei@gmail.com",
+                    password: "jordi1234",
+                    favoriteTechniques: [
+                        "613140ccf8aab565c3e56081",
+                    ],
+                    techniquesProvided: [],
+                },
+                description: "Esta técnica es excelente para revisar...",
+                score: 5,
+        }],
+        userProvider: "6134eec9c158ae21f6cd95d4" 
+        }],
+
+    currentUser: {
+        _id: "612cdf2cfa97f2e5c4f1f107",
+                    name:"Jordi",
+                    profession: "Sensei",
+                    city: "Vic",
+                    avatar: "https://i.ibb.co/GPnrGDw/Default-profile-picture-avatar-photo-placeholder-Vector-illustration.jpg",
+                    email: "jordi.sensei@gmail.com",
+                    password: "jordi1234",
+                    favoriteTechniques: [
+                        "613140ccf8aab565c3e56081",
+                    ],
+                    techniquesProvided: [],
+                    token: "hfdkisdfoisdfk490323423hj423bjk5245b3453k4534",
+                    refreshToken: "j234kj345bhkj34b6hk34jb6oi89v0sf8'0ds9fsjwrjr"
+    },
+
+    isUserAuthenticate: true,
+   
+    token: "hfdkisdfoisdfk490323423hj423bjk5245b3453k4534",
+  
+    refreshToken: "j234kj345bhkj34b6hk34jb6oi89v0sf8'0ds9fsjwrjr",
+   
+    resources: [{
+        _id: "612e614936dee7e13db1db27",
+        title: "Pedagogía de la liberación",
+        author: "Paulo Freire",
+        editorial: "Campana",
+        siteEditorial: "https://editorialCampana.es/"
+    }],
+};
+
+export default stateMock;
