70cc942b4322257a4c5096d4a5a3392226963f5e
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-09-10 20:40:32 +0200

    Add state properties to const state

--- frontend/src/store/state.ts
+++ frontend/src/store/state.ts
@@ -1,14 +1,177 @@
 import {State} from '@/types/interfaces';
 
 const state = {
-    techniques: [],
-    currentTechnique: {},
-    currentUserTechniquesProvided: [],
-    currentUser: {},
+    techniques: [{
+        _id: "",
+        name: "",
+        type: "",
+        time: 0,
+        goal: "",
+        material: "",
+        task: "",
+        workflow: "",
+        ilustration: "",
+        reviews: [{
+                _id: "",
+                technique: {
+                    _id: "",
+                    name: "",
+                    type: "",
+                    time: 0,
+                    goal: "",
+                    material: "",
+                    task: "",
+                    workflow: "",
+                    ilustration: "",
+                    reviews: [
+                        "",
+                    ],
+                },
+                user: {
+                    _id: "",
+                    name:"",
+                    profession: "",
+                    city: "",
+                    avatar: "",
+                    email: "",
+                    password: "",
+                    favoriteTechniques: [
+                        "",
+                    ],
+                    techniquesProvided: [],
+                },
+                description: "",
+                score: 5,
+        }],
+        userProvider: "" 
+        }],
+
+    currentTechnique: {
+        _id: "",
+        name: "",
+        type: "",
+        time: 0,
+        goal: "",
+        material: "",
+        task: "",
+        workflow: "",
+        ilustration: "",
+        reviews: [{
+                _id: "",
+                technique: {
+                    _id: "",
+                    name: "",
+                    type: "",
+                    time: 25,
+                    goal: "",
+                    material: "",
+                    task: "",
+                    workflow: "",
+                    ilustration: "",
+                    reviews: [
+                        "",
+                    ],
+                },
+                user: {
+                    _id: "",
+                    name:"",
+                    profession: "",
+                    city: "",
+                    avatar: "",
+                    email: "",
+                    password: "",
+                    favoriteTechniques: [
+                        "",
+                    ],
+                    techniquesProvided: [
+                        "",
+                    ],
+                },
+                description: "",
+                score: 5,
+        }],
+        userProvider: ""
+    },
+
+    currentUserTechniquesProvided: [{
+        _id: "",
+        name: "",
+        type: "",
+        time: 30,
+        goal: "",
+        material: "",
+        task: "",
+        workflow: "",
+        ilustration: "",
+        reviews: [{
+                _id: "",
+                technique: {
+                    _id: "",
+                    name: "",
+                    type: "",
+                    time: 25,
+                    goal: "",
+                    material: "",
+                    task: "",
+                    workflow: "",
+                    ilustration:"",
+                    reviews: [
+                        "",
+                    ],
+                },
+                user: {
+                    _id: "",
+                    name:"",
+                    profession: "",
+                    city: "",
+                    avatar: "",
+                    email: "",
+                    password: "",
+                    favoriteTechniques: [
+                        "",
+                    ],
+                    techniquesProvided: [
+                        "",
+                    ],
+                },
+                description: "",
+                score: 5,
+        }],
+        userProvider: "" 
+        }],
+
+    currentUser: {
+        _id: "",
+                    name:"",
+                    profession: "",
+                    city: "",
+                    avatar: "",
+                    email: "",
+                    password: "",
+                    favoriteTechniques: [
+                        "",
+                    ],
+                    techniquesProvided: [
+                        "",
+                    ],
+                    token: "",
+                    refreshToken: ""
+
+    },
+
     isUserAuthenticate: false,
+
     token: "",
+
     refreshToken: "",
-    resources: [],
+
+    resources: [{
+            _id: "",
+            title: "",
+            author: "",
+            editorial: "",
+            siteEditorial: ""
+    }],
 };
 
 export default state as unknown as State; 
