697239e67714a1c3fd6fc7b8d67d9d41c36b50a5
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-09-10 21:39:07 +0200

    Create mutations.ts fila and move mutations from index.ts

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,73 +1,18 @@
 import { createStore } from 'vuex';
 import state from '@/store/state';
 import actions from '@/store/actions';
-
+import mutations from '@/store/mutations';
 
 export default createStore({
   state,
-  mutations: {
-    
-    loginUser(state, payload) {
-      state.isUserAuthenticate = true;
-      state.token = payload.token;
-      state.refreshToken = payload.refreshToken;
-    },
-    
-    loadUser(state, payload) {
-      state.currentUser = payload;
-    },
-
-    logoutUser(state, payload) {
-      state.isUserAuthenticate = false;
-      state.token = payload.token;
-      state.refreshToken = payload.refreshToken;
-    },
-
-    updateUserTechniquesProvided(state, payload) {
-      state.currentUser.techniquesProvided.push(payload);
-    },
-
-    updateUserFavoriteTechniques(state, payload) {
-      state.currentUser.favoriteTechniques.push(payload);
-    },
-
-    updateFavoriteTechniques(state, payload) {
-      state.currentUser.favoriteTechniques = payload;
-    },
-
-    loadTechniqueReview(state, payload) {
-      state.currentTechnique.reviews.push(payload);
-},
-
-    loadTechniques(state, payload) {
-      state.techniques = payload;
-    },
-
-    loadOneTechnique(state, payload) {
-      state.currentTechnique = payload;
-    },
-
-    loadCurrentUserTechniquesProvided(state, payload) {
-      state.currentUserTechniquesProvided = payload;
-    },
-
-    loadResources(state, payload) {
-      state.resources = payload;
-    },
-  },
+  mutations,
   actions,
   getters: {
-    // isTechniqueInFavorites:
-    // (state) => (id: Technique) => state.currentUser.favoriteTechniques
-    //   .some(favoriteTechniquesId => favoriteTechniquesId === id),
-  
     isTechniqueInFavorites(state) {
       const favorita = state.currentUser.favoriteTechniques.some((favoriteTechnique) => favoriteTechnique._id === state.currentTechnique._id);
       return favorita;
-      
     }
-  
-  },
+    },
   modules: {
   },
 });
