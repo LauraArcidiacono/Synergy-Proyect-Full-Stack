d802bac774fb2abb830f41f8edf2899f289c81bf
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-09-13 18:08:31 +0200

    Add mock and test

--- frontend/tests/unit/Techniques.spec.ts
+++ frontend/tests/unit/Techniques.spec.ts
@@ -1,24 +1,35 @@
 import { mount } from '@vue/test-utils';
 import Techniques from '@/views/Techniques.vue';
 import router from '@/router';
+import state from '../mockedState';
+import actions from '@/store/actions';
+import mutations from '@/store/mutations';
 
 describe('Given a Techniques component', () => {
     describe('When is rendered', () => {
         test('Then should render a section with the class "techniquesList__search"', () => {
             const wrapper = mount(Techniques, {
                 global: {
-                    plugins: [router],
-                    mocks: {
-                        $store: {
-                            state: {
-                                techniques: [],
-                                isAuthenticate: true,
-                            }
-                        }
-                    }
+                  plugins: [router],
+                  mocks: {
+                    $store: {
+                      state,
+                      getters: {
+                        isTechniqueInFavorites: false,
+                      },
+                      actions, 
+                      mutations,
+                      commit: jest.fn(),
+                      dispatch: jest.fn(),
+                    },
+                    methods: {
+                        mounted: jest.fn(),
+                    }, 
+                    data: jest.fn(),
+                  },
                 },
-            })
-            expect(wrapper.attributes().class).toBe('techniquesList__search')
+              })
+            expect(wrapper.text()).toContain('Lista de Técnicas disponibles')
         })
 
     })
