913d8f50dc8b5a1f9d13ac78d56e4a75d29042dd
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-09-13 23:38:00 +0200

    Add mock to Dashboard test

--- frontend/tests/unit/Dashboard.spec.ts
+++ frontend/tests/unit/Dashboard.spec.ts
@@ -2,45 +2,35 @@ import { mount } from '@vue/test-utils';
 import Dashboard from '../../src/views/Dashboard.vue';
 import router from '@/router/index';
 import state from '../mockedState';
-import actions from '@/store/actions';
-import mutations from '@/store/mutations';
 
 describe('Given a Dashboard component', () => {
     describe('When is rendered', () => {
         test('Then should render an article with the class "articles__techniques"', () => {
             const wrapper = mount(Dashboard, {
-                global: {
-                  plugins: [router],
-                  mocks: {
-                    $store: {
-                      state,
-                      getters: {
-                        isTechniqueInFavorites: false,
-                      }
-                    },
-                  },
+              global: {
+                plugins: [router],
+                mocks: {
+                  $store: {
+                    state,
+                  }, 
+                  data: jest.fn(),
                 },
-              });
+              },
+            });
             expect(wrapper.html()).toContain('<article class="articles__techniques">') 
         })
         test('Then should render an article with the tex "Recursos"', () => {
             const wrapper = mount(Dashboard, {
-                global: {
-                  plugins: [router],
-                  mocks: {
-                    $store: {
-                      state,
-                      getters: {
-                        isTechniqueInFavorites: false,
-                      },
-                      actions, 
-                      mutations,
-                      commit: jest.fn(),
-                      dispatch: jest.fn(),
-                    },
-                  },
+              global: {
+                plugins: [router],
+                mocks: {
+                  $store: {
+                    state,
+                  }, 
+                  data: jest.fn(),
                 },
-              });
+              },
+            });
             expect(wrapper.text()).toContain('Recursos') 
         })
     })
