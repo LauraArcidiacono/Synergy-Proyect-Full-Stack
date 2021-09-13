5e5409f08dee090a8972eab4ce4ff74c96f0cd4f
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-09-13 23:39:44 +0200

    Add mock to Register test

--- frontend/tests/unit/Register.spec.ts
+++ frontend/tests/unit/Register.spec.ts
@@ -1,14 +1,43 @@
 import { mount } from '@vue/test-utils';
 import Register from '@/components/Register.vue';
-
+import router from '@/router';
+import state from '../mockedState';
 describe('Given a Register component', () => {
     describe('When is rendered', () => {
         test('Then should render a div with the class "register__userData"', () => {
-            const wrapper = mount(Register)
+            const wrapper = mount(Register, {
+                global: {
+                    plugins: [router],
+                    mocks: {
+                        $store: {
+                            state,
+                            methods: {
+                                handleSubmit: jest.fn(),
+                                this: jest.fn(),
+                            },
+                        }
+                    }
+                },
+
+            })
             expect(wrapper.html()).toContain('<div class="register__userData">')
         })
         test('Then should render a h3 with the text "Regístrate"', () => {
-            const wrapper = mount(Register)
+            const wrapper = mount(Register, {
+                global: {
+                    plugins: [router],
+                    mocks: {
+                        $store: {
+                            state,
+                            methods: {
+                                handleSubmit: jest.fn(),
+                                this: jest.fn(),
+                            },
+                        }
+                    }
+                },
+
+            })
             expect(wrapper.text()).toContain('Regístrate')
         })
     })
