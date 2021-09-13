26960220f078c56e23704582cf27bff047bec559
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-09-13 18:09:20 +0200

    Finish test to Dashboard.ts

--- frontend/tests/unit/Dashboard.spec.ts
+++ frontend/tests/unit/Dashboard.spec.ts
@@ -1,14 +1,46 @@
 import { mount } from '@vue/test-utils';
 import Dashboard from '../../src/views/Dashboard.vue';
+import router from '@/router/index';
+import state from '../mockedState';
+import actions from '@/store/actions';
+import mutations from '@/store/mutations';
 
 describe('Given a Dashboard component', () => {
     describe('When is rendered', () => {
         test('Then should render an article with the class "articles__techniques"', () => {
-            const wrapper = mount(Dashboard);
+            const wrapper = mount(Dashboard, {
+                global: {
+                  plugins: [router],
+                  mocks: {
+                    $store: {
+                      state,
+                      getters: {
+                        isTechniqueInFavorites: false,
+                      }
+                    },
+                  },
+                },
+              });
             expect(wrapper.html()).toContain('<article class="articles__techniques">') 
         })
         test('Then should render an article with the tex "Recursos"', () => {
-            const wrapper = mount(Dashboard);
+            const wrapper = mount(Dashboard, {
+                global: {
+                  plugins: [router],
+                  mocks: {
+                    $store: {
+                      state,
+                      getters: {
+                        isTechniqueInFavorites: false,
+                      },
+                      actions, 
+                      mutations,
+                      commit: jest.fn(),
+                      dispatch: jest.fn(),
+                    },
+                  },
+                },
+              });
             expect(wrapper.text()).toContain('Recursos') 
         })
     })
