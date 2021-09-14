1996321d6a5ad311d3d4ba95daa32e6fe5a08ddf
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-09-14 19:29:21 +0200

    Fix mistake in axios mock

--- frontend/tests/unit/actions.spec.ts
+++ frontend/tests/unit/actions.spec.ts
@@ -6,17 +6,15 @@ import {
   configActionContextAndState,
   configActionContext,
   configActionContextDispatch,
-  localStorageMock,
 } from '../test-utils';
 import mockedState from '../mockedState';
 
 jest.mock('axios');
-const mockedAxios = axios as jest.Mocked<typeof axios>;
+const mockedAxios = axios as unknown as jest.Mock;
 const commit = jest.fn() as jest.MockedFunction<Commit>;
 const dispatch = jest.fn() as jest.MockedFunction<Dispatch>;
 
 
-let mockStorage = {};
 let user = {
     user: {
         _id: "612cdf2cfa97f2e5c4f1f107",
@@ -40,7 +38,7 @@ describe('Given an object of actions', () => {
         describe('And the action register is executed successfully', () => {
             test('Register should call dispatch', async () => {
 
-                mockedAxios.post.mockResolvedValue({
+                mockedAxios.mockResolvedValue({
                     data: {user: mockedState.currentUser},
                   });
               
@@ -54,7 +52,7 @@ describe('Given an object of actions', () => {
 
         describe('And the action login is executed successfully', () => {
             test('Login should call dispatch', async () => {
-                mockedAxios.post.mockResolvedValue({
+                mockedAxios.mockResolvedValue({
                     data: {},
                 });
 
@@ -79,7 +77,7 @@ describe('Given an object of actions', () => {
         describe('And the action userLogedFromApi is executed successfully', () => {
             
             test('Then userLogedFromApi should call axios.get', async () => {
-                mockedAxios.get.mockResolvedValue({
+                mockedAxios.mockResolvedValue({
                     data: {user: mockedState.currentUser},
                 });  
      
@@ -89,7 +87,7 @@ describe('Given an object of actions', () => {
                 
             })
             test('Then userLogedFromApi should call dispatch', async () => {
-                mockedAxios.get.mockResolvedValue({
+                mockedAxios.mockResolvedValue({
                     data: {},
                 });  
      
@@ -101,7 +99,7 @@ describe('Given an object of actions', () => {
             ////////////////////falla
             test('userLogedFromApi should call commit', async () => {
                 
-                mockedAxios.get.mockResolvedValue({
+                mockedAxios.mockResolvedValue({
                     _id: "",
                     name: "",
                     profession: "",
@@ -120,7 +118,7 @@ describe('Given an object of actions', () => {
             })
 
             test('userLogedFromApi should call commit', async () => {
-                mockedAxios.get.mockResolvedValue({});
+                mockedAxios.mockResolvedValue({});
               
               
                 await actions.userLogedFromApi(configActionContext(commit), user);
@@ -132,7 +130,7 @@ describe('Given an object of actions', () => {
 
         describe('And the action fetchTechniquesFromApi is executed successfully', () => {
             test('Action fetchTechniquesFromApi should call commit', async () => {
-                mockedAxios.get.mockResolvedValue({
+                mockedAxios.mockResolvedValue({
                     data: {}
                   });
               
