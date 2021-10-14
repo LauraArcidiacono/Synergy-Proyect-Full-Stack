1eb158147c18e277a296e6f09a95f668c07b3409
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-10-14 11:55:33 +0200

    Change import in Header test

--- frontend/tests/unit/Header.spec.ts
+++ frontend/tests/unit/Header.spec.ts
@@ -1,7 +1,7 @@
 import { mount } from '@vue/test-utils';
 import Header from '@/components/Header.vue';
 import router from '@/router';
-import state from '@/store/index'
+import state from '../mockedState'
 describe('Given a Header component', () => {
         describe('When is rendered', () => {
             test('Then should render the logo', () => {
