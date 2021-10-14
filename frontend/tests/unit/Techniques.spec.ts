8e28278c15900bd45b1751fbf0124823e58faf1e
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-10-14 12:32:02 +0200

    Reduce technical debt
    -Refactor Techniques test

--- frontend/tests/unit/Techniques.spec.ts
+++ frontend/tests/unit/Techniques.spec.ts
@@ -2,35 +2,22 @@ import { mount } from '@vue/test-utils';
 import Techniques from '@/views/Techniques.vue';
 import router from '@/router';
 import state from '../mockedState';
-import actions from '@/store/actions';
-import mutations from '@/store/mutations';
 
 describe('Given a Techniques component', () => {
     describe('When is rendered', () => {
         test('Then should render a section with the class "techniquesList__search"', () => {
             const wrapper = mount(Techniques, {
-                global: {
-                  plugins: [router],
-                  mocks: {
-                    $store: {
-                      state,
-                      getters: {
-                        isTechniqueInFavorites: false,
-                      },
-                      actions, 
-                      mutations,
-                      commit: jest.fn(),
-                      dispatch: jest.fn(),
-                    },
-                    methods: {
-                        mounted: jest.fn(),
-                    }, 
-                    data: jest.fn(),
-                  },
-                },
-              })
+              global: {
+                plugins: [router],
+                mocks: {
+                  $store: {
+                    state,
+                    dispatch: jest.fn(),
+                  }
+                }
+              }
+            })
             expect(wrapper.text()).toContain('Lista de Técnicas disponibles')
         })
-
     })
 })
