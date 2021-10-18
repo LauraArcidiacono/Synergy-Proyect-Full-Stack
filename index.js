503b020b9265b303e4bf0b399535bfd5272dcbe5
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-10-18 14:34:25 +0200

    Refactor server to Heroku deploy
    -Add server use history and serveStatic
    -Modify routes to avoid overlaps, changing /synergy with /api

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const morgan = require('morgan');
 const passport = require('passport');
 const cors = require('cors');
 const chalk = require('chalk');
+const history = require('connect-history-api-fallback');
+const serveStatic = require('serve-static');
+const path = require('path');
 
 require('dotenv').config();
 require('./src/configDDBB/configDDBB');
@@ -23,15 +26,18 @@ const techniqueProtectedRoutes = require('./src/routes/techniqueRouter');
 const reviewProtectedRoutes = require('./src/routes/reviewRoutes');
 const resourceProtectedRoutes = require('./src/routes/resourceRouter');
 
-server.use('/synergy/auth/', authRoutes);
+server.use('/api/auth/', authRoutes);
 
-server.use('/synergy/users', passport.authenticate('jwt', { session: false }), userProtectedRoutes);
+server.use('/api/users', passport.authenticate('jwt', { session: false }), userProtectedRoutes);
 
-server.use('/synergy/technique', passport.authenticate('jwt', { session: false }), techniqueProtectedRoutes);
+server.use('/api/technique', passport.authenticate('jwt', { session: false }), techniqueProtectedRoutes);
 
-server.use('/synergy/review', passport.authenticate('jwt', { session: false }), reviewProtectedRoutes);
+server.use('/api/review', passport.authenticate('jwt', { session: false }), reviewProtectedRoutes);
 
-server.use('/synergy/resource', passport.authenticate('jwt', { session: false }), resourceProtectedRoutes);
+server.use('/api/resource', passport.authenticate('jwt', { session: false }), resourceProtectedRoutes);
+
+server.use(history());
+server.use('/', serveStatic(path.join(__dirname, '/frontend/dist')));
 
 server.listen(
   port,
