b22e12f39a27369355c188cb0a1b9b491b0884f4
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-10-31 17:27:08 +0100

    Add setData and handleFilterTechniques test

--- frontend/tests/unit/Techniques.spec.ts
+++ frontend/tests/unit/Techniques.spec.ts
@@ -20,4 +20,45 @@ describe('Given a Techniques component', () => {
             expect(wrapper.text()).toContain('Lista de Técnicas disponibles')
         })
     })
+
+    describe('And a category is choosen', () => {
+      test('Then should setData with the choosen category', () => {
+        const wrapper = mount(Techniques, {
+          global: {
+            plugins: [router],
+            mocks: {
+              $store: {
+                state,
+                dispatch: jest.fn(),
+              },
+            },
+          },
+        })
+
+        wrapper.setData({filterValue: 'confianza'})
+
+        expect(wrapper.vm.filterValue).toBe('confianza') 
+      })
+
+      test('Then filterTechniques to have been called', () => {
+        const wrapper = mount(Techniques, {
+          global: {
+            plugins: [router],
+            mocks: {
+              $store: {
+                state,
+                dispatch: jest.fn(),
+              },
+            },
+          },
+        })
+
+        const filterTechniques = jest.fn();
+        filterTechniques();
+        const dropedCategoryMenu = wrapper.get('[data-test="selectCategory"]');
+        dropedCategoryMenu.trigger('change')
+        
+        expect(filterTechniques).toHaveBeenCalled()
+      })
+    })
 })
